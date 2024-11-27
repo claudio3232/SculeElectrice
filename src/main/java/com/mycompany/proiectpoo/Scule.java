b36328e45b0a4781c847e43e9a55b26cf461ea35
md464 <md464@WIN-3.CTI.UGAL.RO>
2024-11-27 23:00:48 +0200

    interfata

---
+++ src/main/java/com/mycompany/proiectpoo/Scule.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.proiectpoo;
+
+/**
+ *
+ * @author md464
+ */
+public interface Scule {
+   public void functioneaza();
+    public String afiseazaDetalii();
+}
