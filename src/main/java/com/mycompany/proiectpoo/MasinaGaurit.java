1bf4b1262c08bb3090689eebb7eeade52c0f67ec
md464 <md464@WIN-3.CTI.UGAL.RO>
2024-11-27 22:59:08 +0200

    Masina

--- src/main/java/com/mycompany/proiectpoo/MasinaGaurit.java
+++ src/main/java/com/mycompany/proiectpoo/MasinaGaurit.java
@@ -10,29 +10,43 @@ package com.mycompany.proiectpoo;
  */
 
 public class MasinaGaurit extends SculeElectrice {
-    private long numarPercutii; // numarul de percutii pe minut
+    private int numarPercutii; // numarul de percutii pe minut
+     private boolean functiePercutie; // Dacă are sau nu funcție de percuție
+    private int diametruMaximGaurire; // Diametrul maxim de găurire în mm
+    private String tipMandrina; // Tipul mandrinei (automat, manual)
+
 
     // Constructor fara argumente
     public MasinaGaurit() {
         super();
         this.numarPercutii = 0;
+         this.numarPercutii = 0;
+        this.functiePercutie = false;
+        this.diametruMaximGaurire = 0;
+        this.tipMandrina = "Necunoscut";
     }
 
     // Constructor cu toate argumentele
-    public MasinaGaurit(String firma, int putere, float greutate, long numarPercutii) {
+    public MasinaGaurit(String firma, int putere, float greutate, int numarPercutii, boolean functiePercutie, int diametruMaximGaurire, String tipMandrina) {
         super(firma, putere, greutate);
         this.numarPercutii = numarPercutii;
+        this.functiePercutie = functiePercutie;
+        this.diametruMaximGaurire = diametruMaximGaurire;
+        this.tipMandrina = tipMandrina;
     }
 
     // Constructor de copiere
     public MasinaGaurit(MasinaGaurit masina) {
         super(masina);
         this.numarPercutii = masina.numarPercutii;
+         this.functiePercutie = masina.functiePercutie;
+        this.diametruMaximGaurire = masina.diametruMaximGaurire;
+        this.tipMandrina = masina.tipMandrina;
     }
 
     // Metoda toString
+    @Override
     public String toString() {
-        return super.toString() + ", Numar percutii: " + numarPercutii + " pe minut";
+        return super.toString() + ", Numar percutii: " + numarPercutii + " pe minut"+", Functie percutie: " + functiePercutie +", Diametru maxim gaurire: " + diametruMaximGaurire + " mm" +", Tip mandrina: " + tipMandrina;
     }
 }
-
