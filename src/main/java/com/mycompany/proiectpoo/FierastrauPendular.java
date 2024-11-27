64b96af8cd6dd5814f8316066138b6ff52fade88
md464 <md464@WIN-3.CTI.UGAL.RO>
2024-11-27 21:27:03 +0200

    fierastrau

--- src/main/java/com/mycompany/proiectpoo/FierastrauPendular.java
+++ src/main/java/com/mycompany/proiectpoo/FierastrauPendular.java
@@ -4,27 +4,38 @@ package com.mycompany.proiectpoo;
  * Clasa FierastrauPendular derivata din SculeElectrice.
  */
 class FierastrauPendular extends SculeElectrice {
-    private int vitezaTaiere; // Viteza de taiere in RPM
+    protected int vitezaTaiere; 
+    protected boolean vitezaReglabila;
+    protected boolean laserghidaj;
 
     // Constructor fara argumente
     public FierastrauPendular() {
         super();
         this.vitezaTaiere = 0;
+        this.vitezaReglabila = false;
+        this.laserghidaj = false;
     }
 
     // Constructor cu toate argumentele
-    public FierastrauPendular(String firma, int putere, float greutate, int vitezaTaiere) {
+    public FierastrauPendular(String firma, int putere, float greutate, int vitezaTaiere, boolean vitezaReglabila ,boolean laserghida ) {
         super(firma, putere, greutate);
         this.vitezaTaiere = vitezaTaiere;
+        this.vitezaReglabila = vitezaReglabila;
+        this.laserghidaj = laserghidaj;
     }
 
-    // Implementare metoda utilizarePrincipala
-    public String utilizarePrincipala() {
-        return "Fierastrau pendular este utilizat pentru taierea precisa a lemnului si metalului.";
+        public FierastrauPendular(FierastrauPendular fierastrau) {
+        super(fierastrau);
+        this.vitezaTaiere = fierastrau.vitezaTaiere;
+        this.vitezaReglabila = fierastrau.vitezaReglabila;
+        this.laserghidaj = fierastrau.laserghidaj;
     }
+
 
     // Implementare metoda descriereDetaliata
     public String toString() {
-        return super.toString() + ", Viteza taiere: " + vitezaTaiere + " RPM";
+        return super.toString() + ", Viteza taiere: " + vitezaTaiere + " RPM, " + 
+                "viteza reglabila: " + (vitezaReglabila ? "Da" : "Nu") + ", " +
+                "laserghidaj: " + (laserghidaj ? "Da" : "Nu");
     }
 }
