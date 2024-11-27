0333ec32afeb5f9435707caa18beb6b322608f23
md464 <md464@WIN-3.CTI.UGAL.RO>
2024-11-27 21:28:15 +0200

    surubelnita

--- src/main/java/com/mycompany/proiectpoo/SurubelnitaElectrica.java
+++ src/main/java/com/mycompany/proiectpoo/SurubelnitaElectrica.java
@@ -4,27 +4,42 @@ package com.mycompany.proiectpoo;
  * Clasa SurubelnitaElectrica derivata din SculeElectrice.
  */
 class SurubelnitaElectrica extends SculeElectrice {
-    private int turatiiMaxime; // Turatii maxime în RPM
+    protected int nrviteze;
+    protected float autonomie;
+    protected String tipAlimentare;
 
     // Constructor fara argumente
     public SurubelnitaElectrica() {
         super();
-        this.turatiiMaxime = 0;
+        this.nrviteze = 0;
+        this.autonomie = 0.0f;
+        this.tipAlimentare = "";
     }
 
     // Constructor cu toate argumentele
-    public SurubelnitaElectrica(String firma, int putere, float greutate, int turatiiMaxime) {
+    public SurubelnitaElectrica(String firma, int putere, float greutate, int nrviteze, float autonomie, String tipAlimentare ) {
         super(firma, putere, greutate);
-        this.turatiiMaxime = turatiiMaxime;
+        this.nrviteze = nrviteze;
+        this.autonomie = autonomie;
+        this.tipAlimentare = tipAlimentare;
     }
 
-    // Implementare metoda utilizarePrincipala
-    public String utilizarePrincipala() {
-        return "Surubelnita electrica este folosita pentru insurubare rapida si eficienta.";
+       public SurubelnitaElectrica(SurubelnitaElectrica surubelnita) {
+        super(surubelnita);
+        this.nrviteze = surubelnita.nrviteze;
+        this.autonomie = surubelnita.autonomie;
+        this.autonomie = surubelnita.autonomie;
     }
+    @Override
+public String specificatii() {
+    return "Surubelnita electrica de la firma " + firma + ", cu puterea " + putere + "W, greutate " + greutate + " kg.";
+}
+
 
     // Implementare metoda descriereDetaliata
     public String toString() {
-        return super.toString() + ", Turatii maxime: " + turatiiMaxime + " RPM";
+        return super.toString() + ", Numar viteza: " + nrviteze + " trepte, " +
+                "cu autonomie de: " + autonomie + " ore, " +
+                "si tip de alimentare cu: " + tipAlimentare;
     }
 }
